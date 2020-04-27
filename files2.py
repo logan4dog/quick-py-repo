b6f29854ae9ae1a12639467c23354b184d28a6c9
James Z. Logan Jr <logan4dog@gmail.com>
2020-04-27 15:30:08 -0400

    first commit

---
+++ files2.py
@@ -0,0 +1,15 @@
+f = open('textfiles.txt')
+h = list()
+for line in f:
+    h.append(line.split())
+
+for item in h:
+    field1 = item[4]+'\t'
+    field2 = item[8]+'\t'
+    field3 = item[5]+' '+item[6]+' '+item[7]
+    if len(field1) < 15:
+        print(len(field1))
+    else:
+        print(len(field1))
+
+    print("{0} {1} {2} ".format(field1,field2,field3)
